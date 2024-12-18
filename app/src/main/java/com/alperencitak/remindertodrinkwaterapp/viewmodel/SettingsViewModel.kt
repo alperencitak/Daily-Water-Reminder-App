2a49ccdf58d855c338689e6a460422e1c0ad4561
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-12-19 02:28:34 +0300

    View Model class written

---
+++ app/src/main/java/com/alperencitak/remindertodrinkwaterapp/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,77 @@
+package com.alperencitak.remindertodrinkwaterapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alperencitak.remindertodrinkwaterapp.model.Settings
+import com.alperencitak.remindertodrinkwaterapp.model.TimeInterval
+import com.alperencitak.remindertodrinkwaterapp.repository.SettingsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val settingsRepository: SettingsRepository
+) : ViewModel() {
+    private val _settings = MutableStateFlow<Settings?>(null)
+    val settings: StateFlow<Settings?> = _settings
+
+    init {
+        loadSettings()
+    }
+
+    private fun loadSettings(){
+        viewModelScope.launch {
+            try {
+                settingsRepository.settings.collect{ settingsData ->
+                    _settings.value = settingsData
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun toggleDarkMode(){
+        viewModelScope.launch {
+            try {
+                if(_settings.value != null){
+                    settingsRepository.updateDarkMode(!_settings.value!!.isDarkMode)
+                    loadSettings()
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun toggleSleepMode(){
+        viewModelScope.launch {
+            try {
+                if(_settings.value != null){
+                    settingsRepository.updateSleepMode(!_settings.value!!.isSleepMode)
+                    loadSettings()
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateTimeInterval(timeInterval: TimeInterval){
+        viewModelScope.launch {
+            settingsRepository.updateTimeInterval(timeInterval)
+            loadSettings()
+        }
+    }
+
+    fun updateWaterQuantity(quantity: Float){
+        viewModelScope.launch {
+            settingsRepository.updateWaterQuantity(quantity)
+            loadSettings()
+        }
+    }
+
+}
