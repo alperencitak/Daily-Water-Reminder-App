c0746958c7cc9ed286548dbc3aebc5a4e5732745
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-12-19 02:27:41 +0300

    Application installation done

--- app/src/main/java/com/alperencitak/remindertodrinkwaterapp/MainActivity.kt
+++ app/src/main/java/com/alperencitak/remindertodrinkwaterapp/MainActivity.kt
@@ -3,45 +3,18 @@ package com.alperencitak.remindertodrinkwaterapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.alperencitak.remindertodrinkwaterapp.ui.theme.ReminderToDrinkWaterAppTheme
+import com.alperencitak.remindertodrinkwaterapp.view.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ReminderToDrinkWaterAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReminderToDrinkWaterAppTheme {
-        Greeting("Android")
     }
 }
