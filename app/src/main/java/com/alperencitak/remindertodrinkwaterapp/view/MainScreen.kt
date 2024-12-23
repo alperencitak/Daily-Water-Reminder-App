f6aa6e17d50d718bd74efad40220b2d9ffdff60a
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-12-23 19:29:32 +0300

    Face named Bob added to main menu

--- app/src/main/java/com/alperencitak/remindertodrinkwaterapp/view/MainScreen.kt
+++ app/src/main/java/com/alperencitak/remindertodrinkwaterapp/view/MainScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -35,13 +36,20 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.alperencitak.remindertodrinkwaterapp.R
 import com.alperencitak.remindertodrinkwaterapp.ui.theme.BabyBlue
 import com.alperencitak.remindertodrinkwaterapp.ui.theme.SeafoamGreen
@@ -61,7 +69,23 @@ fun MainScreen(paddingValues: PaddingValues) {
     val currentMinute = Calendar.getInstance().get(Calendar.MINUTE)
     var drinkingGlass by remember { mutableIntStateOf(((currentHour - 9) * 60) + currentMinute / intervalMinutes) }
 
-    Box(modifier = Modifier.fillMaxSize()){
+    val bobMap = mapOf(
+        stringResource(R.string.bob_lol) to R.drawable.bob_lol,
+        stringResource(R.string.bob_angel) to R.drawable.bob_angel,
+        stringResource(R.string.bob_surprise) to R.drawable.bob_surprise,
+        stringResource(R.string.bob_cry) to R.drawable.bob_cry,
+        stringResource(R.string.bob_anime) to R.drawable.bob_anime,
+        stringResource(R.string.bob_crazy) to R.drawable.bob_crazy,
+        stringResource(R.string.bob_love) to R.drawable.bob_love,
+        stringResource(R.string.bob_neutral) to R.drawable.bob_neutral,
+        stringResource(R.string.bob_sleep) to R.drawable.bob_sleep
+    )
+
+    val nunito = FontFamily(
+        Font(R.font.nunito_black, FontWeight.Normal)
+    )
+
+    Box(modifier = Modifier.fillMaxSize()) {
         Image(
             painter = painterResource(id = R.drawable.bg3),
             contentDescription = "Background Image",
@@ -69,16 +93,39 @@ fun MainScreen(paddingValues: PaddingValues) {
             contentScale = ContentScale.Crop
         )
     }
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 64.dp, horizontal = 32.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val bobEntry = bobMap.entries.random()
+        Image(
+            painter = painterResource(bobEntry.value),
+            contentDescription = "Cry Bob Gif",
+            modifier = Modifier
+                .size(75.dp),
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            text = bobEntry.key,
+            fontSize = 17.sp,
+            fontFamily = nunito,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
     Column(
-        modifier = Modifier.padding(paddingValues).fillMaxSize().padding(horizontal = 64.dp, vertical = 128.dp),
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+            .padding(horizontal = 64.dp, vertical = 128.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "${drinkingGlass*200}ml / ${waterQuantity}ml",
+            text = "${drinkingGlass * 200}ml / ${waterQuantity}ml",
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp,
             color = Color.Black,
+            fontFamily = nunito,
             modifier = Modifier.padding(bottom = 12.dp)
         )
         LazyVerticalGrid(
@@ -86,13 +133,16 @@ fun MainScreen(paddingValues: PaddingValues) {
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            items(goalGLass){ index ->
-                val alpha = if(index+1 > drinkingGlass) 0.5f else 1f
+            items(goalGLass) { index ->
+                val alpha = if (index + 1 > drinkingGlass) 0.5f else 1f
                 Image(
                     painter = painterResource(id = R.drawable.glassofwater),
                     contentDescription = "Glass Icon",
-                    modifier = Modifier.width(48.dp).height(48.dp).alpha(alpha),
-                    contentScale = ContentScale.Crop
+                    modifier = Modifier
+                        .width(48.dp)
+                        .height(48.dp)
+                        .alpha(alpha),
+                    contentScale = ContentScale.Fit
                 )
             }
         }
