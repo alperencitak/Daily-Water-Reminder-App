750d7dc38c785b3f6c2267ce12964466d8d01092
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2024-12-23 22:29:25 +0300

    Unnecessary imports deleted

--- app/src/main/java/com/alperencitak/remindertodrinkwaterapp/view/MainScreen.kt
+++ app/src/main/java/com/alperencitak/remindertodrinkwaterapp/view/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.alperencitak.remindertodrinkwaterapp.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,13 +14,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -33,10 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
@@ -44,16 +34,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.alperencitak.remindertodrinkwaterapp.R
-import com.alperencitak.remindertodrinkwaterapp.ui.theme.BabyBlue
-import com.alperencitak.remindertodrinkwaterapp.ui.theme.SeafoamGreen
-import com.alperencitak.remindertodrinkwaterapp.ui.theme.WaterBlue
 import com.alperencitak.remindertodrinkwaterapp.viewmodel.SettingsViewModel
 import java.util.Calendar
 
