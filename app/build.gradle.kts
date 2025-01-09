3abb0e97f56e0d2eb0f7cc7fe575c005d03866e5
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-09 19:50:52 +0300

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.alperencitak.remindertodrinkwaterapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["ADMOB_APP_ID"] = ADMOB_APP_ID
         buildConfigField("String", "ADMOB_AD_UNIT_ID", ADMOB_AD_UNIT_ID)
