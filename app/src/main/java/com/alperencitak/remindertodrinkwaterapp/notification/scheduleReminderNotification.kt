13c5610b7133e2d837c141b79074e5e655ec488c
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-04-27 21:22:13 +0300

    Added log

--- app/src/main/java/com/alperencitak/remindertodrinkwaterapp/notification/scheduleReminderNotification.kt
+++ app/src/main/java/com/alperencitak/remindertodrinkwaterapp/notification/scheduleReminderNotification.kt
@@ -4,9 +4,13 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import java.util.Calendar
+import java.util.Date
 
 fun scheduleReminderNotification(context: Context, intervalMinutes: Int) {
+    Log.d("WaterReminder", "scheduleReminderNotification çağrıldı. Aralık: $intervalMinutes dakika")
+
     val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
     val intent = Intent(context, NotificationReceiver::class.java)
@@ -19,6 +23,7 @@ fun scheduleReminderNotification(context: Context, intervalMinutes: Int) {
 
     val interval = intervalMinutes * 60 * 1000L
     val triggerTime = System.currentTimeMillis() + interval
+    Log.d("WaterReminder", "Alarm ayarlanıyor. Tetikleme zamanı: ${Date(triggerTime)}, Aralık: $interval ms")
 
     alarmManager.setRepeating(
         AlarmManager.RTC_WAKEUP,
@@ -26,4 +31,5 @@ fun scheduleReminderNotification(context: Context, intervalMinutes: Int) {
         interval,
         pendingIntent
     )
+    Log.d("WaterReminder", "Alarm başarıyla ayarlandı")
 }
